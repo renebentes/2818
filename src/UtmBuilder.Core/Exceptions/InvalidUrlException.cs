7d33ed155cc677b0a318421573a7826b0e9141f2
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-03-27 15:35:03 -0300

    refactor: Aplica uso de Source Generators para construção da Regex

--- src/UtmBuilder.Core/Exceptions/InvalidUrlException.cs
+++ src/UtmBuilder.Core/Exceptions/InvalidUrlException.cs
@@ -2,11 +2,9 @@ using System.Text.RegularExpressions;
 
 namespace UtmBuilder.Core.Exceptions;
 
-public class InvalidUrlException : Exception
+public partial class InvalidUrlException : Exception
 {
     private const string DefaultErrorMessage = "Invalid URL";
-    private const string UrlRegexPattern =
-        @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
 
     public InvalidUrlException(string message = DefaultErrorMessage)
         : base(message)
@@ -17,7 +15,10 @@ public class InvalidUrlException : Exception
         if (string.IsNullOrEmpty(address))
             throw new InvalidUrlException(message);
 
-        if (!Regex.IsMatch(address, UrlRegexPattern))
+        if (!UrlRegex().IsMatch(address))
             throw new InvalidUrlException(message);
     }
+
+    [GeneratedRegex("^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$")]
+    private static partial Regex UrlRegex();
 }
