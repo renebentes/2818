ee6e8742e32f5521508614ecbe3be10f4ba7a0c6
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-03-27 15:30:15 -0300

    refactor: Valida URLs com Exception

--- src/UtmBuilder.Core/Exceptions/InvalidUrlException.cs
+++ src/UtmBuilder.Core/Exceptions/InvalidUrlException.cs
@@ -1,9 +1,23 @@
+using System.Text.RegularExpressions;
+
 namespace UtmBuilder.Core.Exceptions;
 
 public class InvalidUrlException : Exception
 {
-    public InvalidUrlException(string? message = "Invalid URL")
+    private const string DefaultErrorMessage = "Invalid URL";
+    private const string UrlRegexPattern =
+        @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
+
+    public InvalidUrlException(string message = DefaultErrorMessage)
         : base(message)
+    { }
+
+    public static void ThrowIfInvalid(string address, string message = DefaultErrorMessage)
     {
+        if (string.IsNullOrEmpty(address))
+            throw new InvalidUrlException(message);
+
+        if (!Regex.IsMatch(address, UrlRegexPattern))
+            throw new InvalidUrlException(message);
     }
 }
--- src/UtmBuilder.Core/ValueObjects/Url.cs
+++ src/UtmBuilder.Core/ValueObjects/Url.cs
@@ -5,9 +5,6 @@ namespace UtmBuilder.Core.ValueObjects;
 
 public sealed class Url : ValueObject
 {
-    private const string UrlRegexPattern =
-        @"^(http|https):(\\/\\/www\\.|\\/\\/www\\.|\\/\\/|\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$|(http|https):(\\/\\/localhost:\\d*|\\/\\/127\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\\/.*)?$";
-    
     /// <summary>
     /// Create a new URL
     /// </summary>
@@ -16,10 +13,7 @@ public sealed class Url : ValueObject
     {
         Address = address;
 
-        if (Regex.IsMatch(Address, UrlRegexPattern))
-        {
-            throw new InvalidUrlException();
-        }
+        InvalidUrlException.ThrowIfInvalid(Address);
     }
 
     /// <summary>
