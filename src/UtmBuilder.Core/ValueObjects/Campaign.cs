7213383c2b318a67b09121fee26d973af79d3292
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-03-27 10:36:49 -0300

    docs: Documenta API dos VOs

--- src/UtmBuilder.Core/ValueObjects/Campaign.cs
+++ src/UtmBuilder.Core/ValueObjects/Campaign.cs
@@ -2,6 +2,15 @@ namespace UtmBuilder.Core.ValueObjects;
 
 public sealed class Campaign : ValueObject
 {
+    /// <summary>
+    /// Generate a new campaign for a URL
+    /// </summary>
+    /// <param name="source">The referrer (e.g. google, newsletter)</param>
+    /// <param name="medium">Marketing medium (e.g. cpc, banner, email)</param>
+    /// <param name="name">Product, promo code, or slogan (e.g. spring_sale) One of campaign name or campaign id are required.</param>
+    /// <param name="id">The ads campaign id.</param>
+    /// <param name="term">Identify the paid keywords</param>
+    /// <param name="content">Use to differentiate ads</param>
     public Campaign(
         string source,
         string medium,
@@ -18,15 +27,33 @@ public sealed class Campaign : ValueObject
         Term = term;
     }
 
+    /// <summary>
+    /// Use to differentiate ads
+    /// </summary>
     public string? Content { get; }
 
+    /// <summary>
+    /// The ads campaign id.
+    /// </summary>
     public string? Id { get; }
 
+    /// <summary>
+    /// Marketing medium (e.g. cpc, banner, email)
+    /// </summary>
     public string Medium { get; }
 
+    /// <summary>
+    /// Product, promo code, or slogan (e.g. spring_sale) One of campaign name or campaign id are required.
+    /// </summary>
     public string Name { get; }
 
+    /// <summary>
+    /// The referrer (e.g. google, newsletter)
+    /// </summary>
     public string Source { get; }
 
+    /// <summary>
+    /// Identify the paid keywords
+    /// </summary>
     public string? Term { get; }
 }
