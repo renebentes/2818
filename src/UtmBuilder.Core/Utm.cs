0c12284b79777f9854a198b385cd35413eccf756
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-03-27 10:51:04 -0300

    feat: Define a entidade UTM

---
+++ src/UtmBuilder.Core/Utm.cs
@@ -0,0 +1,30 @@
+using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core;
+
+/// <summary>
+/// Represent a Urchin Tracking Module - UTM
+/// </summary>
+public sealed class Utm
+{
+    /// <summary>
+    /// Create a new UTM
+    /// </summary>
+    /// <param name="url">URL (Website Link)</param>
+    /// <param name="campaign">Campaign Details</param>
+    public Utm(Url url, Campaign campaign)
+    {
+        Url = url;
+        Campaign = campaign;
+    }
+    
+    /// <summary>
+    /// Campaign Details
+    /// </summary>
+    public Campaign Campaign { get; }
+
+    /// <summary>
+    /// URL (Website Link)
+    /// </summary>
+    public Url Url { get; }
+}
