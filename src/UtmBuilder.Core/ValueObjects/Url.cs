059a86803ec5a9529b369cd2815fe264bf88c9bc
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-04-03 13:04:03 -0300

    feat: Implementa implicit operators para VO Url

--- src/UtmBuilder.Core/ValueObjects/Url.cs
+++ src/UtmBuilder.Core/ValueObjects/Url.cs
@@ -19,4 +19,27 @@ public sealed class Url : ValueObject
     /// Address of URL (Website link)
     /// </summary>
     public string Address { get; }
+
+    /// <summary>
+    /// Converts a website <paramref name="address"/> to an <see cref="Url"/> object 
+    /// </summary>
+    /// <param name="address">The website address</param>
+    /// <returns>Returns an <see cref="Url"/> object</returns>
+    public static implicit operator Url(string address)
+        => new(address);
+    
+    /// <summary>
+    /// Converts an <paramref name="url"/> object to a website address
+    /// </summary>
+    /// <param name="url">The Url object</param>
+    /// <returns>Returns a website address</returns>
+    public static implicit operator string(Url url)
+        => url.ToString();
+
+    /// <summary>
+    /// Returns a website address that represents an <see cref="Url"/> object
+    /// </summary>
+    /// <returns>Returns the website <see cref="Url.Address"/></returns>
+    public override string ToString() 
+        => Address;
 }
