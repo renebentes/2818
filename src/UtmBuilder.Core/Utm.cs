4b59be6692a5f19bfe8bc21823717924fddf1860
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-04-03 10:52:24 -0300

    feat: Obtém uma URL no padrão UTM (Urchin Tracking Module)

--- src/UtmBuilder.Core/Utm.cs
+++ src/UtmBuilder.Core/Utm.cs
@@ -1,3 +1,4 @@
+using UtmBuilder.Core.Extensions;
 using UtmBuilder.Core.ValueObjects;
 
 namespace UtmBuilder.Core;
@@ -27,4 +28,17 @@ public sealed class Utm
     /// URL (Website Link)
     /// </summary>
     public Url Url { get; }
+
+    public override string ToString()
+    {
+        var segments = new List<string>();
+        segments.AddIfNotNull("utm_source", Campaign.Source);
+        segments.AddIfNotNull("utm_medium", Campaign.Medium);
+        segments.AddIfNotNull("utm_campaign", Campaign.Name);
+        segments.AddIfNotNull("utm_id", Campaign.Id);
+        segments.AddIfNotNull("utm_term", Campaign.Term);
+        segments.AddIfNotNull("utm_content", Campaign.Content);
+
+        return $"{Url}?{string.Join('&', segments)}";
+    }
 }
